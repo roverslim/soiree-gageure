e9127fc6c8010e15bf176d24ed67989e9a182a68
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-01-21 19:53:59 -0500

    Generalize locale tests
    The test suite needs not depends on the value of I18n.defaul_locale.

--- spec/helpers/lotteries_helper_spec.rb
+++ spec/helpers/lotteries_helper_spec.rb
@@ -3,47 +3,43 @@
 require 'rails_helper'
 
 RSpec.describe(LotteriesHelper, type: :helper) do
+  include I18nSpecHelper
+
   let(:lottery) do
     Lottery.create!(event_date: Time.zone.today)
   end
 
   describe('#lock_or_unlock') do
-    it('returns "Lock registration" when lottery#locked == false') do
-      assert_equal(
-        'Lock registration',
-        lock_or_unlock(lottery),
-      )
+    context('when lottery#locked == false') do
+      it('returns "Lock registration" when locale: en') do
+        with_locale(:en) do
+          assert_equal('Lock registration', lock_or_unlock(lottery))
+        end
+      end
+
+      it("returns 'Fermer l'enregistrement' when locale: fr") do
+        with_locale(:fr) do
+          assert_equal("Fermer l'enregistrement", lock_or_unlock(lottery))
+        end
+      end
     end
 
-    it('returns "Unlock registration" when lottery#locked == true') do
-      lottery.update!(locked: true)
-      assert_equal(
-        'Unlock registration',
-        lock_or_unlock(lottery),
-      )
-    end
-  end
+    context('when lottery#locked == true') do
+      before(:each) do
+        lottery.update!(locked: true)
+      end
 
-  describe('#lock_or_unlock under I18n.locale = :fr') do
-    around(:each) do |example|
-      I18n.locale = :fr
-      example.run
-      I18n.locale = :en
-    end
+      it('returns "Unlock registration" when locale: en') do
+        with_locale(:en) do
+          assert_equal('Unlock registration', lock_or_unlock(lottery))
+        end
+      end
 
-    it('returns "Fermer l\'enregistrement" when lottery#locked == false') do
-      assert_equal(
-        "Fermer l'enregistrement",
-        lock_or_unlock(lottery),
-      )
-    end
-
-    it('returns "Réouvrir l\'enregistrement" when lottery#locked == true') do
-      lottery.update!(locked: true)
-      assert_equal(
-        "Réouvrir l'enregistrement",
-        lock_or_unlock(lottery),
-      )
+      it("returns 'Réouvrir l'enregistrement' when lottery#locked == true") do
+        with_locale(:fr) do
+          assert_equal( "Réouvrir l'enregistrement", lock_or_unlock(lottery))
+        end
+      end
     end
   end
 end
