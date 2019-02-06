fb140ba0b0ac6ee06877d373d94fe14f6a7a2c26
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-02-06 18:56:35 -0500

    Test localized strings in the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,26 +13,58 @@ RSpec.describe(User, type: :model) do
   describe('#valid?') do
     it('requires :email to be present') do
       new_user = User.new
-      new_user.valid?
-      expect(new_user.errors[:email]).to include("can't be blank")
+
+      with_locale(:en) do
+        new_user.valid?
+        expect(new_user.errors[:email]).to include("can't be blank")
+      end
+
+      with_locale(:fr) do
+        new_user.valid?
+        expect(new_user.errors[:email]).to include("ne peut pas être vide")
+      end
     end
 
     it('requires :email to be unique') do
       new_user = User.new(email: user.email)
-      new_user.valid?
-      expect(new_user.errors[:email]).to include('has already been taken')
+
+      with_locale(:en) do
+        new_user.valid?
+        expect(new_user.errors[:email]).to include('has already been taken')
+      end
+
+      with_locale(:fr) do
+        new_user.valid?
+        expect(new_user.errors[:email]).to include('a déjà été assigné')
+      end
     end
 
     it('requires :password to be present on create') do
       new_user = User.new
-      new_user.valid?
-      expect(new_user.errors[:password]).to include("can't be blank")
+
+      with_locale(:en) do
+        new_user.valid?
+        expect(new_user.errors[:password]).to include("can't be blank")
+      end
+
+      with_locale(:fr) do
+        new_user.valid?
+        expect(new_user.errors[:password]).to include("ne peut pas être vide")
+      end
     end
 
     it('requires :password to be at least 3 chars long on create') do
       new_user = User.new(password: 'a')
-      new_user.valid?
-      expect(new_user.errors[:password]).to include('is too short (minimum is 3 characters)')
+
+      with_locale(:en) do
+        new_user.valid?
+        expect(new_user.errors[:password]).to include('is too short (minimum is 3 characters)')
+      end
+
+      with_locale(:fr) do
+        new_user.valid?
+        expect(new_user.errors[:password]).to include('est trop court (minimum de 3 charactères)')
+      end
 
       new_user.password = 'foo'
       new_user.valid?
@@ -46,8 +78,16 @@ RSpec.describe(User, type: :model) do
 
     it('requires :password to be at least 3 chars long on update') do
       user.password = 'a'
-      user.valid?
-      expect(user.errors[:password]).to include('is too short (minimum is 3 characters)')
+
+      with_locale(:en) do
+        user.valid?
+        expect(user.errors[:password]).to include('is too short (minimum is 3 characters)')
+      end
+
+      with_locale(:fr) do
+        user.valid?
+        expect(user.errors[:password]).to include('est trop court (minimum de 3 charactères)')
+      end
 
       user.password = 'foo'
       user.valid?
